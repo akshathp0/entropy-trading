bdc788bf6fbfd8c83fb2a0e95e6b135f5fef1196
akshathp0 <akshath@pasam.com>
2026-03-04 22:22:59 -0500

    initial backtest done

---
+++ strategy/backtest.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import yaml
+
+with open("config.yml", "r") as file:
+    config = yaml.safe_load(file)
+
+def run_backtest(df):
+    df['Pct Return'] = df['SPY'].pct_change()
+    df['SPY Return Curve'] = (1 + df['Pct Return']).cumprod()
+
+    df['Strategy Return'] = df['Exposure'].shift(1) * df['Pct Return']
+    df['Strategy Return'] = df['Strategy Return'].fillna(0)
+
+    df['Equity Curve'] = (1 + df['Strategy Return']).cumprod()
+
+    return df
