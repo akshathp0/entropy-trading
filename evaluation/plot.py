709174f9468774aa563ce2e706eebc537d9e3b53
akshathp0 <akshath@pasam.com>
2026-03-18 11:24:22 -0400

    plot pca bars

--- evaluation/plot.py
+++ evaluation/plot.py
@@ -9,3 +9,12 @@ def plot_correlation_heatmap(matrix):
 
     return fig
 
+def plot_pca_bars(loadings, n_components):
+    fig, axes = plt.subplots(n_components, 1, figsize = (12, n_components * 5))
+    for i, ax in enumerate(axes):
+        sns.barplot(loadings[f'PC{i + 1}'], ax = ax)
+        ax.set_title(f'PC{i + 1} Bars')
+
+    plt.tight_layout()
+
+    return fig
